97b386fd449d876b65d833777aae88bbb22ab2e1
ijlee2 <ijlee2@users.noreply.github.com>
2020-10-15 11:35:06 -0500

    Added script

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello world!');
+
+if (process.env.KEY_TO_UNIVERSE) {
+  console.log('I can do anything.');
+}
+
+// After forking the repo, uncomment the line, commit changes, and make a pull request.
+// console.log(`The key to the universe is ${process.env.KEY_TO_UNIVERSE}.`);
