99df0439197275a2ab2f76e49400e45ae2395a08
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2020-10-15 11:49:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ if (process.env.KEY_TO_UNIVERSE) {
   console.log('I can do anything.');
 }
 
-// After forking the repo, uncomment the line, commit changes, and make a pull request.
+// After forking the repo:
+//
+//  - uncomment line 14
+//  - commit changes
+//  - push commit (check if GitHub Actions ran)
+//  - make a pull request (check again if GitHub Actions ran)
+//
 // console.log(`The key to the universe is ${process.env.KEY_TO_UNIVERSE}.`);
